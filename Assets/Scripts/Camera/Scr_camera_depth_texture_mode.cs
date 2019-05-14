86c38c7dde62737e1bf75aa9c40a24d943a25bee
Penol99 <migen9930@gmail.com>
2019-05-14 20:46:37 +0200

    water

---
+++ Assets/Scripts/Camera/Scr_camera_depth_texture_mode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_camera_depth_texture_mode : MonoBehaviour
+{
+    [SerializeField]
+    DepthTextureMode depthTextureMode;
+
+    private void OnValidate()
+    {
+        SetCameraDepthTextureMode();
+    }
+
+    private void Awake()
+    {
+        SetCameraDepthTextureMode();
+    }
+
+    private void SetCameraDepthTextureMode()
+    {
+        GetComponent<Camera>().depthTextureMode = depthTextureMode;
+    }
+}
